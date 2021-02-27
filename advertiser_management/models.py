8236227fcd5e10513bdc3ba293df3a5748f3c465
Shayan80MZ <shayan80sama@yahoo.com>
2021-02-28 01:53:43 +0330

    create Ad and Advertiser models

--- advertiser_management/models.py
+++ advertiser_management/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 
+class Advertiser(models.Model):
+    advertiser_id = models.IntegerField()
+    name = models.CharField(max_length=100)
+    clicks = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+
+
+class Ad(models.Model):
+    ad_id = models.IntegerField()
+    title = models.CharField(max_length=100)
+    imgUrl = models.ImageField()
+    link = models.URLField()
+    clicks = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
