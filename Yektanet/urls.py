1ae95b1bc725378d37e2b1635598e4fc5aa8ad62
Shayan80MZ <shayan80sama@yahoo.com>
2021-03-01 16:11:43 +0330

    edit ads.html and edit CountClick url

--- Yektanet/urls.py
+++ Yektanet/urls.py
@@ -2,12 +2,10 @@ from django.contrib import admin
 from django.urls import path
 
 from advertiser_management import views
-#
-# app_name = "advertiser_management"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('show-ads/', views.showAds, name='show-ads'),
-    path('<int:pk>/', views.CountClickAndRedirect.as_view(), name='count-click'),
+    path('click/<int:pk>/', views.CountClickAndRedirect.as_view(), name='count-click'),
     path('create-ad/', views.AdFormView.as_view(), name='create-ad')
 ]
