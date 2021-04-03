d04a3357a834ea95ee58205c30d30ef2546c6d73
Shayan80MZ <shayan80sama@yahoo.com>
2021-04-03 10:47:12 +0430

    edit tasks.py

--- advertiser_management/tasks.py
+++ advertiser_management/tasks.py
@@ -6,14 +6,14 @@ from datetime import datetime
 @task(name='hourly_task')
 def saveHourlyTask():
     for ad in Ad.objects.all():
-        clickNum = ad.click_set.filter(click_date__gte=datetime.now().replace(hour=datetime.now().hour - 1)).count()
-        viewNum = ad.view_set.filter(view_date__gte=datetime.now().replace(hour=datetime.now().hour - 1)).count()
+        clickNum = ad.click_set.filter(click_date__gte=datetime.now().replace(hour=datetime.now().hour - 1, minute=0, second=0)).count()
+        viewNum = ad.view_set.filter(view_date__gte=datetime.now().replace(hour=datetime.now().hour - 1, minute=0, second=0)).count()
         HourlyData.objects.create(ad=ad, clicksNum=clickNum, viewsNum=viewNum, date=datetime.now())
 
 
 @task(name='daily_task')
 def saveDailyTask():
     for ad in Ad.objects.all():
-        clickNum = ad.click_set.filter(click_date__gte=datetime.now().replace(day=datetime.now().day - 1)).count()
-        viewNum = ad.view_set.filter(view_date__gte=datetime.now().replace(day=datetime.now().day - 1)).count()
+        clickNum = ad.click_set.filter(click_date__gte=datetime.now().replace(day=datetime.now().day - 1, minute=0, second=0)).count()
+        viewNum = ad.view_set.filter(view_date__gte=datetime.now().replace(day=datetime.now().day - 1, minute=0, second=0)).count()
         DailyData.objects.create(ad=ad, clicksNum=clickNum, viewsNum=viewNum, date=datetime.now())
