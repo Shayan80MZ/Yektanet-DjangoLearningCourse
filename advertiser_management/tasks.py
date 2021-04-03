520f26665e58bd9e9ad322afb9c1da51b4fa8c6b
Shayan80MZ <shayan80sama@yahoo.com>
2021-04-03 10:54:23 +0430

    finalize tasks.py

--- advertiser_management/tasks.py
+++ advertiser_management/tasks.py
@@ -16,4 +16,4 @@ def saveDailyTask():
     for ad in Ad.objects.all():
         clickNum = ad.click_set.filter(click_date__gte=datetime.now().replace(day=datetime.now().day - 1, minute=0, second=0)).count()
         viewNum = ad.view_set.filter(view_date__gte=datetime.now().replace(day=datetime.now().day - 1, minute=0, second=0)).count()
-        DailyData.objects.create(ad=ad, clicksNum=clickNum, viewsNum=viewNum, date=datetime.now().replace(minute=0, second=0))
+        DailyData.objects.create(ad=ad, clicksNum=clickNum, viewsNum=viewNum, date=datetime.now().replace(hour=datetime.now().hour - 1, minute=0, second=0))
